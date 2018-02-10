eeade311e907b062c5a749873d87f5c49777909c
Iddan Aharonson <mail@iddan.co>
2018-02-10 13:46:58 +0200

    better animation

--- src/components/LayersVisualization.js
+++ src/components/LayersVisualization.js
@@ -1,5 +1,26 @@
-import React from 'react'
+import React, { cloneElement, Component } from 'react'
+import { darken } from 'polish'
 import './LayersVisualization.css'
+
+class TimeoutProps extends Component {
+  
+  state = {}
+
+  componentWillMount() {
+    const { timeout = 0, ...rest } = this.props
+    this.timeout = setTimeout(() => {
+      this.setState(rest)
+    }, timeout) 
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
+  }
+
+  render() {
+    return cloneElement(this.props.children, this.state)
+  }
+}
 
 const Layer = ({ children, z, color, darkColor }) => (
   <figure className="layer" style={{ transform: `translateZ(${z}px)` }}>
@@ -18,8 +39,12 @@ const LayersVisualization = () => (
   <div className="LayersVisualization">
     <figure className="layers">
       <Layer z={0} color="green" darkColor="darkgreen">1</Layer>
-      <Layer z={50} color="cyan" darkColor="teal">2</Layer>
-      <Layer z={100} color="red" darkColor="darkred">3</Layer>
+      <TimeoutProps z={50}>
+        <Layer z={0} color="cyan" darkColor="teal">2</Layer>
+      </TimeoutProps>
+      <TimeoutProps z={100}>
+        <Layer z={0} color="red" darkColor="darkred">3</Layer>
+      </TimeoutProps>
     </figure>
   </div>
 )
