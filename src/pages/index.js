1c8e182075cbdc105117ac85bd6487e8e95eeece
Iddan Aharonson <mail@iddan.co>
2018-03-11 23:40:40 +0200

    call to action

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,10 +49,11 @@ const IndexPage = () => (
       Unlike banners AdLayer’s ads don’t take valuable space from the site, it actually benefits more content being presented leveraging it for displaying more ads. Because the ads are displayed according to the site’s media and not keywords in the text each ad is more relevant to the user. Also, AdLayer’s web plugin is optimized for more modern browsers serving as little code as possible using newer web technologies.
       </p>
     </section>
-    <section>
+    <section className="greyed">
       <p>
       By now we already have a working demo and looking for partners, advertisers and publishers, to launch a pilot and test our product in the wild. According to previous research we are expecting significant engagement and now we want to assure it in the wild.
       </p>
+      <CallToAction />
     </section>
   </div>
 );
