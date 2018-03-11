5edbc65a0a1ac5530ce178a81e8bbb6a4554af59
Iddan Aharonson <mail@iddan.co>
2018-03-12 00:12:54 +0200

    better paragraph by @liorsbg & Hay

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "gatsby-link";
 import HeroVisualization from "../components/HeroVisualization";
 import CallToAction from "../components/CallToAction";
-import demo from '../assets/demo.png'
+import demo from "../assets/demo.png";
 import "./index.css";
 import LayersVisualization, { Layer } from "../components/LayersVisualization";
 
@@ -36,24 +36,30 @@ const IndexPage = () => (
     </section>
     <section>
       <p>
-        To display the ads our lightweight site plugin detects the major media
-        in the publisher’s site and send them to our servers. There using simple
-        mechanism we detect tags to be used to match relevant ads. If the image
-        was already processed the and the ad still relevant the ad will
-        immediately serve the relevant ad. The plugin gets the relevant ads and
-        display them in the matching content while preserving the site’s
-        behaviour.
+        Built with the most advanced web technologies and backed by cutting edge
+        AI, our lightweight site plugin efficiently detects the major media in
+        the publisher’s site and enriches it with relevant content, all while
+        preserving the sites look, feel and behavior.
       </p>
       <img className="demo" src={demo} alt="demo" />
     </section>
     <section>
       <p>
-      Unlike banners AdLayer’s ads don’t take valuable space from the site, it actually benefits more content being presented leveraging it for displaying more ads. Because the ads are displayed according to the site’s media and not keywords in the text each ad is more relevant to the user. Also, AdLayer’s web plugin is optimized for more modern browsers serving as little code as possible using newer web technologies.
+        Unlike banners AdLayer’s ads don’t take valuable space from the site, it
+        actually benefits more content being presented leveraging it for
+        displaying more ads. Because the ads are displayed according to the
+        site’s media and not keywords in the text each ad is more relevant to
+        the user. Also, AdLayer’s web plugin is optimized for more modern
+        browsers serving as little code as possible using newer web
+        technologies.
       </p>
     </section>
     <section className="greyed">
       <p>
-      By now we already have a working demo and looking for partners, advertisers and publishers, to launch a pilot and test our product in the wild. According to previous research we are expecting significant engagement and now we want to assure it in the wild.
+        By now we already have a working demo and looking for partners,
+        advertisers and publishers, to launch a pilot and test our product in
+        the wild. According to previous research we are expecting significant
+        engagement and now we want to assure it in the wild.
       </p>
       <CallToAction />
     </section>
